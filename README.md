d6d27bf44d5ac7e022d52217ee6d139247040a2b
Martin Stephen Cox <admin@martinstephencox.com>
2019-04-19 13:36:15 +0100

    Unify database scripts and made it simpler to use

--- README.md
+++ README.md
@@ -6,8 +6,7 @@ Currently it's just a testbed for some React/Express/mongodb stuff
 - Install mongodb from https://www.mongodb.com/download-center/community (not as a service)
 - Add mongo's bin folder to the Path environment variable https://stackoverflow.com/questions/15053893/error-when-trying-to-connect-to-a-mongod
 - Run "npm ci" in root directory
-- Run "npm run database:start" to start the database
-- In a separate command line window run "npm run database:populate" to populate database with test data
+- Run "npm run start:database -- --new=true" to start the database and populate it with test data.
 - In a separate command line window run "npm run build:watch"
 - In a separate command line window run "node build/server.js"
 - Open http://localhost:5000/build/ in your browser to see the client and interact with it
--- src/tools/Start.ts
+++ src/tools/Start.ts
@@ -1,20 +1,148 @@
 import * as child from "child_process";
 import * as fs from "fs-extra";
+import * as yargs from "yargs";
+import * as mongodb from "mongodb";
 
-const directory = "build/database";
+const args = yargs
+	.option(
+		"new",
+		{
+			alias: "n",
+			description: "Whether the database is being installed for the first time (or if an existing installation should be overwritten).",
+			default: false
+		}
+	).option(
+		"directory",
+		{
+			alias: "d",
+			description: "The database installation directory.",
+			default: "C:\\Temp\\database"
+		}
+	).option(
+		"port",
+		{
+			alias: "p",
+			description: "The database server port.",
+			default: 27017
+		}
+	).option(
+		"name",
+		{
+			description: "The database name.",
+			default: "test"
+		}
+	)
+	.argv;
 
-/** Starts the mongo database server. */
-function main(): void {
+const newInstall = args.new;
+const directory = args.directory;
+const port = args.port;
+const name = args.name;
+const serverUrl = `mongodb://localhost:${port}`
+
+if (newInstall) {
+	console.log(`Starting new installation at "${directory}"`);
+
 	if (fs.existsSync(directory)) {
 		console.log("Removing existing directory");
-		fs.rmdirSync(directory);
+		fs.removeSync(directory);
 	};
 
 	console.log("Creating new directory");
 	fs.mkdirSync(directory);
-
-	console.log("Starting database server");
-	child.execSync(`cd build && mongod --dbpath=database`);
 }
 
-main();
+console.log(`Starting database server at "${serverUrl}"`);
+child.spawn(`cd build && mongod --dbpath=${directory}`, { shell: true });
+
+if (newInstall) {
+	console.log("New install detected, populating with test data.");
+	populate();
+} else {
+	console.log("Existing database detected.");
+	read();
+}
+
+/**
+ * Populates the mongo database with test data.
+ */
+async function populate(): Promise<void> {
+	const client = new mongodb.MongoClient(serverUrl, { useNewUrlParser: true });
+
+	console.log("Connecting to database server");
+	try {
+		await client.connect();
+	} catch {
+		console.log("Failed to connect to database server");
+		return;
+	}
+
+	console.log("Successfully connected to database server");
+	console.log(`Creating database "${name}"`);
+	const database = client.db(name);
+
+	let collection: mongodb.Collection;
+
+	console.log("Successfully created database");
+	console.log("Creating collection");
+	try {
+		collection = await database.createCollection("people");
+	} catch {
+		console.log("Failed to create collection");
+		return;
+	}
+
+	console.log("Successfully created collection");
+	console.log("Inserting documents");
+	try {
+		await collection.insertMany([{
+			name: "Tom"
+		}, {
+			name: "Dick"
+		}, {
+			name: "Harry"
+		}]);
+	} catch {
+		console.log("Failed to insert documents");
+		return;
+	}
+
+	console.log("Successfully inserted documents");
+
+	read(client);
+}
+
+ /**
+  * Reads test data from the mongo database.
+  * @param client The optional database client.
+  */
+async function read(client?: mongodb.MongoClient): Promise<void> {
+	if (!client) {
+		client = new mongodb.MongoClient(serverUrl, { useNewUrlParser: true });
+
+		await client.connect();
+	}
+
+	console.log(name);
+	const database = client.db(name);
+	const collection = database.collection("people");
+
+	let result: any[] = [];
+
+	console.log("Finding document");
+	try {
+		result = await collection.find({ name: "Dick" }).toArray();
+	} catch {
+		console.log("Error finding document");
+		return;
+	}
+
+	if (result.length === 1) {
+		console.log("Found document:");
+		console.log(result[0]);
+	} else {
+		console.log("Failed to find document");
+	}
+
+	client.close();
+}

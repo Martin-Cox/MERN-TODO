2c6e28ffeaf343f193a2f2854bb990c85fb34a25
Martin Stephen Cox <admin@martinstephencox.com>
2019-04-22 11:59:19 +0100

    Remove old test files

--- src/server/main/Main.ts
+++ src/server/main/Main.ts
@@ -1,11 +1,9 @@
 import "dotenv/config";
 
 import { Server } from "../main/Server";
-import { CatFact } from "../controllers/CatFact";
 import { TaskController } from "../controllers/TaskController";
 
 const server = new Server([
-	new CatFact(),
 	new TaskController()
 ], parseInt(process.env.SERVER_PORT), parseInt(process.env.DATABASE_PORT), process.env.DATABASE_NAME);
 
